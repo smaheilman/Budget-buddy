ba573465940a6289c98e41ad925d2fb6dfc66afa
kelbach <87092340+Kelbach@users.noreply.github.com>
2021-11-22 20:09:31 -0500

    api-routes/models

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
+
+//might need to rename homeroutes
--- server.js
+++ server.js
@@ -1,17 +1,45 @@
-const { query } = require('express');
+//npm i express
 const express = require('express');
-const fs = require('fs');
+const routes = require('./controllers');
+//npm i sequelize
+const sequelize = require('./config/connection');
+// npm install --save path
 const path = require('path');
+// npm i express-session connect-session-sequelize
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const PORT = process.env.PORT || 3001;
+//MIGHT USE THESE LATER
+// // npm i handlebars, npm i express-handlebars
+// const exphbs = require('express-handlebars');
+// //imports handlebar helpers
+// const helpers = require('./utils/helpers');
+// const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super duper duper secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.send("Hello World!")
-});
+app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+// turn on connection to db and server
+// force true will always sync to server (reset) upon npm start
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
