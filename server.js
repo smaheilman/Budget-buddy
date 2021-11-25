ebcd7b5876fa83795a822f2d9665d9f7ca91a348
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-11-24 20:27:17 -0500

    landing page is rendered

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const Handlebars = require("handlebars");
-const fs = require('fs');
 const path = require('path');
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+
 
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(routes);
 
-
-app.get('/', (req, res) => {
-  res.send("Hello World!")
-});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
