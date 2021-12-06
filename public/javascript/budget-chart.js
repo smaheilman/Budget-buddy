d6a28c8b8507b69affe061400678766af64dfa31
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-12-06 06:45:53 -0500

    chart renders

---
+++ public/javascript/budget-chart.js
@@ -0,0 +1,29 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+
+let colors = {
+    bills: 'rgb(54, 162, 235)',
+    entertainment: 'rgb(255, 99, 132)',
+    shopping: 'rgb(255, 159, 64)'
+}
+const myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3],
+            backgroundColor: [
+                colors.entertainment,
+                colors.bills,
+                colors.shopping,
+            ],
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
