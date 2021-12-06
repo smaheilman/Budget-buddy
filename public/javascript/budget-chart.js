bdcfc0b3217c901d4dfa8ec44d6e7c8fac728bea
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-12-06 16:27:58 -0500

    second chart

--- public/javascript/budget-chart.js
+++ public/javascript/budget-chart.js
@@ -1,16 +1,66 @@
-const ctx = document.getElementById('myChart').getContext('2d');
 
+const transContext = document.getElementById('trans-chart').getContext('2d');
+const billContext = document.getElementById('bill-chart').getContext('2d');
+
+const transactionEls = $('.transaction-row');
+
+
+let transactionObject = {
+    bills: {
+        trx: [],
+        total: 0
+    },
+    entertainment: {
+        trx: [],
+        total: 0
+    },
+    shopping: {
+        trx: [],
+        total: 0
+    },
+    other: {
+        trx: [],
+        total: 0
+    },
+};
+
+transactionEls.each(function () {
+    let id = $(this).attr('id').split('-')[0]
+    console.log(id)
+    let trxObj = {
+        name: $(`#${id}-name`).text().trim(),
+        amount: parseFloat($(`#${id}-amount`).text().trim().replace('$', '')),
+        date: $(`#${id}-date`).text().trim()
+    }
+    if ($(`#${id}-category`).text().trim() === 'Bills/Utilities') {
+        transactionObject.bills.trx.push(trxObj);
+        transactionObject.bills.total += trxObj.amount;
+    }
+    if ($(`#${id}-category`).text().trim() === 'Entertainment') {
+        transactionObject.entertainment.trx.push(trxObj);
+        transactionObject.entertainment.total += trxObj.amount;
+    }
+    if ($(`#${id}-category`).text().trim() === 'Shopping') {
+        transactionObject.shopping.trx.push(trxObj);
+        transactionObject.shopping.total += trxObj.amount;
+    }
+    if ($(`#${id}-category`).text().trim() === 'Other') {
+        transactionObject.other.trx.push(trxObj);
+        transactionObject.other.total += trxObj.amount;
+    }
+})
+console.log(transactionObject)
 let totals = {
-    bills: $('#bills-total').text().trim(),
-    entertainment: $("#entertainment-total").text().trim(),
-    shopping: $("#shopping-total").text().trim(),
-    other: $("#other-total").text().trim()
+    bills: transactionObject.bills.total,
+    entertainment: transactionObject.entertainment.total,
+    shopping: transactionObject.shopping.total,
+    other: transactionObject.other.total
 }
 
 if (totals.bills + totals.entertainment + totals.shopping + totals.other == 0) {
-    totals.bills++; 
-    totals.entertainment++; 
-    totals.shopping++; 
+    totals.bills++;
+    totals.entertainment++;
+    totals.shopping++;
     totals.other++;
 }
 
@@ -20,12 +70,53 @@ let colors = {
     shopping: 'rgb(255, 159, 64)',
     other: 'rgb(61, 61, 61)'
 }
-const myChart = new Chart(ctx, {
+let transData = {
+    labels: [],
+    data: [],
+    backgroundColor: [
+
+    ]
+}
+transactionObject.bills.trx.forEach(trx => {
+    transData.labels.push(trx.name);
+    transData.data.push(trx.amount);
+    transData.backgroundColor.push(colors.bills)
+});
+transactionObject.entertainment.trx.forEach(trx => {
+    transData.labels.push(trx.name);
+    transData.data.push(trx.amount);
+    transData.backgroundColor.push(colors.entertainment)
+})
+transactionObject.shopping.trx.forEach(trx => {
+    transData.labels.push(trx.name);
+    transData.data.push(trx.amount);
+    transData.backgroundColor.push(colors.shopping)
+})
+transactionObject.other.trx.forEach(trx => {
+    transData.labels.push(trx.name);
+    transData.data.push(trx.amount);
+    transData.backgroundColor.push(colors.other)
+})
+console.log(transData)
+
+const transChart = new Chart(transContext, {
+    type: 'pie',
+    data: {
+        labels: transData.labels,
+        datasets: [{
+            label: 'Transactions',
+            data: transData.data,
+            backgroundColor: transData.backgroundColor
+        }]
+
+    }
+})
+const billChart = new Chart(billContext, {
     type: 'pie',
     data: {
         labels: ['Bills and Utilities', 'Entertainment', 'Shopping', 'Other'],
         datasets: [{
-            label: 'Amount Spent',
+            label: 'Categories',
             data: [totals.bills, totals.entertainment, totals.shopping, totals.other],
             backgroundColor: [
                 colors.bills,
